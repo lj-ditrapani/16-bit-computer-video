7dd553ac5ab21b015a78863f648b46224dc83408
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2014-10-13 20:15:21 -0400

    Update lint.sh file

--- lint.sh
+++ lint.sh
@@ -1,3 +1,4 @@
-coffeelint spec/video-spec.coffee spec/video-test.coffee \
-    video.coffee
+coffeelint spec/independent-spec.coffee spec/populate-ram.coffee \
+           spec/rendering-spec.coffee spec/tile-spec.coffee \
+           spec/video-spec.coffee spec/video-test.coffee video.coffee
 tidy -e video-spec.html video-test.html
